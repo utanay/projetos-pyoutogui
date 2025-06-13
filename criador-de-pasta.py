447cea0bedc1e60723f7346d8bbc35843441bbc4
utanay <adenilsonran@gmail.com>
2025-06-13 09:25:27 -0300

    criando-pasta

---
+++ criador-de-pasta.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+pyautogui.moveTo(210,387,duration=2)
+pyautogui.rightClick(x=210,y=387)
+sleep(1)
+pyautogui.moveTo(266,257,duration=1)
+sleep(1)
+pyautogui.click()
+#pyautogui.moveTo(596,280,duration=1)
