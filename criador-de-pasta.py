485aba7f385f293e163e482432fa96689bcbd4c9
utanay <adenilsonran@gmail.com>
2025-06-14 17:06:27 -0300

    automação de pasta concluida

--- criador-de-pasta.py
+++ criador-de-pasta.py
@@ -1,9 +1,11 @@
 import pyautogui
 from time import sleep
-pyautogui.moveTo(210,387,duration=2)
-pyautogui.rightClick(x=210,y=387)
-sleep(1)
-pyautogui.moveTo(266,257,duration=1)
-sleep(1)
-pyautogui.click()
-#pyautogui.moveTo(596,280,duration=1)
+for i in range (3):
+    pyautogui.moveTo(210,387,duration=2)
+    pyautogui.rightClick(x=210,y=387)
+    sleep(0)
+    pyautogui.moveTo(266,257,duration=1)
+    sleep(0)
+    pyautogui.click()
+    pyautogui.moveTo(648,264,duration=1)
+    pyautogui.click()
