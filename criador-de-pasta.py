55b29f8aa1d1173d0f7f0d07da60c8dbfec5eb7b
utanay <adenilsonran@gmail.com>
2025-06-14 22:13:52 -0300

    envio de currilo v1

--- criador-de-pasta.py
+++ criador-de-pasta.py
@@ -1,11 +1,12 @@
+#def criador_de_pastas():
 import pyautogui
 from time import sleep
-for i in range (3):
-    pyautogui.moveTo(210,387,duration=2)
-    pyautogui.rightClick(x=210,y=387)
-    sleep(0)
-    pyautogui.moveTo(266,257,duration=1)
-    sleep(0)
-    pyautogui.click()
-    pyautogui.moveTo(648,264,duration=1)
-    pyautogui.click()
+for i in range (10):
+     pyautogui.moveTo(210,387,duration=0)
+     pyautogui.rightClick(x=210,y=387)
+     sleep(0)
+     pyautogui.moveTo(266,257,duration=0)
+     sleep(0)
+     pyautogui.click()
+     pyautogui.moveTo(648,264,duration=0)
+     pyautogui.click()
